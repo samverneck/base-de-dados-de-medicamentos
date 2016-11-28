26ee4586b573d63a7e0717b5c6e641dc04cda2ca
aspto <aspto@uol.com.br>
2016-11-28 21:38:33 -0200

    Second commit

--- api/modules/Medicamento/molecular.structure.js
+++ api/modules/Medicamento/molecular.structure.js
@@ -1,6 +1,32 @@
-module.exports = [
-  'name',
-  'email',
-  'description',
-  'created_at'
+
+  module.exports = [
+  		'MED_ABC',
+            'MED_CTR',
+            'MED_LAB',
+            'LAB_NOM',
+            'MED_DESCRICAO',
+            'MED_APRESENTACAO',
+            'MED_PCO18',
+            'MED_PLA18',
+            'MED_FRA18',
+            'MED_PCO17',
+            'MED_PLA17',
+            'MED_FRA17',
+            'MED_PCO12',
+            'MED_PLA12',
+            'MED_FRA12',
+            'MED_UNI',
+            'MED_IPI',
+            'MED_DTVIG',
+            'MED_BARRA',
+            'MED_NEGPOS',
+            'MED_PRINCI',
+            'MED_PCO20',
+            'MED_PLA20',
+            'MED_FRA20',
+            'MED_PCOZFM',
+            'MED_PLAZFM',
+            'MED_FRAZFM',
+            'MED_REGMS',
+            'MESANO'
 ]
